f42f5016af48089ac79beab343c168e03a8b9e26
josue <josue-zu@hotmail.com>
2023-06-17 13:54:44 -0300

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://foodexplorer-api-6vdh.onrender.com",
 });
 
 api.interceptors.request.use(
